27c26081354ce30bb57e21fa886e5144e2d43cbf
Roland Adams <adamsrolly7@gmail.com>
2026-05-16 13:37:55 +0100

    fixed

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\WebhookController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TokenController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 // Public routes (anyone can access)
